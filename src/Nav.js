222e55d9a5a60cf585465797b05473f35be40f9e
Ram lakhan singh <pt2867000@gmail.com>
2022-02-03 02:38:27 +0530

    Netflix-complete-build

---
+++ src/Nav.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from "react";
+import "./Nav.css";
+
+function Nav() {
+  const [show, handleShow] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+  return (
+    <div className={`nav ${show && "nav__black"}`}>
+      <img
+        className="nav__logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png"
+        alt="netflix logo"
+      />
+
+      <img
+        className="nav__avatar"
+        src="https://ih0.redbubble.net/image.618427277.3222/flat,1000x1000,075,f.u1.jpg"
+        alt="netflix avatar"
+      />
+    </div>
+  );
+}
+
+export default Nav;
